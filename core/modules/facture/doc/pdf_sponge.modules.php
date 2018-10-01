5f990113a1cf611afc51eab16c2a2879abd16c76
John BOTELLA <john.botella@atm-consulting.fr>
2018-10-01 16:28:28 +0200

    Fix display retained warranty

--- core/modules/facture/doc/pdf_sponge.modules.php
+++ core/modules/facture/doc/pdf_sponge.modules.php
@@ -179,7 +179,7 @@ class pdf_sponge extends ModelePDFFactures
 	    if (! empty($conf->global->MAIN_USE_FPDF)) $outputlangs->charset_output='ISO-8859-1';
 	    
 	    // Translations
-	    $outputlangs->loadLangs(array("main", "bills", "products", "dict", "companies"));
+	    $outputlangs->loadLangs(array("main", "bills", "products", "dict", "companies", "pdfevolution@pdfevolution"));
 	    
 	    $nblignes = count($object->lines);
 	    
@@ -1162,6 +1162,10 @@ class pdf_sponge extends ModelePDFFactures
 	{
 		global $conf,$mysoc;
 
+		
+		// Translations
+		$outputlangs->loadLangs(array("main", "bills", "products", "dict", "companies", "pdfevolution@pdfevolution"));
+		
         $sign=1;
         if ($object->type == 2 && ! empty($conf->global->INVOICE_POSITIVE_CREDIT_NOTE)) $sign=-1;
 
@@ -1195,7 +1199,7 @@ class pdf_sponge extends ModelePDFFactures
 		    }
 		}
 		if(!empty($i)){
-		  $avancementGlobal = $percent/$i;
+		    $avancementGlobal = $percent/$i;
 		}
 		else{
 		    $avancementGlobal = 0;
@@ -1205,14 +1209,21 @@ class pdf_sponge extends ModelePDFFactures
 		$TPreviousIncoice = $object->tab_previous_situation_invoice;
 		
 		$total_a_payer = 0;
-		foreach ($TPreviousIncoice as &$fac) $total_a_payer += $fac->total_ht;
+		$total_a_payer_ttc = 0;
+		foreach ($TPreviousIncoice as &$fac){
+		    $total_a_payer += $fac->total_ht;
+		    $total_a_payer_ttc += $fac->total_ttc;
+		}
 		$total_a_payer += $object->total_ht;
+		$total_a_payer_ttc += $object->total_ttc;
 		
 		if(!empty($avancementGlobal)){
 		    $total_a_payer = $total_a_payer * 100 / $avancementGlobal;
+		    $total_a_payer_ttc = $total_a_payer_ttc  * 100 / $avancementGlobal;
 		}
 		else{
 		    $total_a_payer = 0;
+		    $total_a_payer_ttc = 0;
 		}
 		
 		$deja_paye = 0;
@@ -1411,7 +1422,14 @@ class pdf_sponge extends ModelePDFFactures
 					}
 
 					if($sum_pdf_tva!=$object->total_tva) { // apply coef to recover the VAT object amount (the good one)
-						$coef_fix_tva = $object->total_tva / $sum_pdf_tva;
+					    if(!empty($sum_pdf_tva))
+					    {
+					        $coef_fix_tva = $object->total_tva / $sum_pdf_tva;
+					    }
+					    else {
+					        $coef_fix_tva = 1;
+					    }
+					   
 
 						foreach($this->tva as $tvakey => $tvaval) {
 							$this->tva[$tvakey]=$tvaval * $coef_fix_tva;
@@ -1533,7 +1551,7 @@ class pdf_sponge extends ModelePDFFactures
 				$pdf->MultiCell($largcol2, $tab2_hl, price($sign * $total_ttc, 0, $outputlangs), $useborder, 'R', 1);
 				
 				
-				if($object->type == Facture::TYPE_SITUATION)
+				/*if($object->type == Facture::TYPE_SITUATION)
 				{
 				    // reste à payer total
 				    $index++;
@@ -1542,10 +1560,51 @@ class pdf_sponge extends ModelePDFFactures
 				    $pdf->SetFillColor(255,255,255);
 				    $pdf->SetXY($col1x, $tab2_top + $tab2_hl * $index);
 				    $pdf->MultiCell($col2x-$col1x, $tab2_hl, $outputlangs->transnoentities('SituationTotalRayToRest'), 0, 'L', 1);
-				    
-				    $total_ht = ($conf->multicurrency->enabled && $object->multicurrency_tx != 1 ? $object->multicurrency_total_ht : $object->total_ht);
 				    $pdf->SetXY($col2x, $tab2_top + $tab2_hl * $index);
-				    $pdf->MultiCell($largcol2, $tab2_hl, price($total_a_payer-$deja_paye-$object->total_ht, 0, $outputlangs), 0, 'R', 1);
+				    $pdf->MultiCell($largcol2, $tab2_hl, price($total_a_payer_ttc-$deja_paye, 0, $outputlangs), 0, 'R', 1);
+				}*/
+				
+				
+				// Retained warranty
+				if( !empty($object->situation_final) &&  ( $object->type == Facture::TYPE_SITUATION && (!empty($object->retained_warranty) ) ) )
+				{
+				    // Check if this situation invoice is 100% for real
+				    if(!empty($object->lines)){
+				        $displayWarranty = true;
+				        foreach( $object->lines as $i => $line ){
+				            if($line->product_type < 2 && $line->situation_percent < 100){
+				                $displayWarranty = false;
+				                break;
+				            }
+				        }
+				    }
+				    
+				    if($displayWarranty){
+				        $pdf->SetTextColor(40,40,40);
+				        $pdf->SetFillColor(255,255,255);
+
+				        $retainedWarranty = $total_a_payer_ttc * $object->retained_warranty / 100;
+				        $billedWithRetainedWarranty = $object->total_ttc - $retainedWarranty ;
+				        
+				        // Billed - retained warranty
+				        $index++;
+				        $pdf->SetXY($col1x, $tab2_top + $tab2_hl * $index);
+				        $pdf->MultiCell($col2x-$col1x, $tab2_hl, $outputlangs->transnoentities("PDFEVOLToPayOn", dol_print_date($object->date_lim_reglement, 'day')), $useborder, 'L', 1);
+				        
+				        $pdf->SetXY($col2x, $tab2_top + $tab2_hl * $index);
+				        $pdf->MultiCell($largcol2, $tab2_hl, price($billedWithRetainedWarranty) , $useborder, 'R', 1);
+				        
+				        // retained warranty
+				        $index++;
+				        $pdf->SetXY($col1x, $tab2_top + $tab2_hl * $index);
+				        
+				        $retainedWarrantyToPayOn = $outputlangs->transnoentities("PDFEVOLRetainedWarranty") . ' ('.$object->retained_warranty.'%)';
+				        $retainedWarrantyToPayOn.=  !empty($object->retained_warranty_date_limit)?' '.$outputlangs->transnoentities("PDFEVOLtoPayOn", dol_print_date($object->retained_warranty_date_limit, 'day')):'';
+				        
+				        $pdf->MultiCell($col2x-$col1x, $tab2_hl, $retainedWarrantyToPayOn, $useborder, 'L', 1);
+				        $pdf->SetXY($col2x, $tab2_top + $tab2_hl * $index);
+				        $pdf->MultiCell($largcol2, $tab2_hl, price($retainedWarranty) , $useborder, 'R', 1);
+				    }
 				}
 				
 			}
@@ -1695,7 +1754,7 @@ class pdf_sponge extends ModelePDFFactures
 		global $conf, $langs;
 		
 		// Translations
-		$outputlangs->loadLangs(array("main", "bills", "propal", "companies"));
+		$outputlangs->loadLangs(array("main", "bills", "propal", "companies", "pdfevolution@pdfevolution"));
 
 		$default_font_size = pdf_getPDFFontSize($outputlangs);
 
